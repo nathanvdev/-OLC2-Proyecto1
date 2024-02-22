c3a825a13a0b8809364d095bfc103e965d7fd2c4
nathanvdevlinux <nathanvaldez413@gmail.com>
2024-02-22 17:24:17 -0600

    datos primitivos

--- backend/interpreter/analyzer/grammar.py
+++ backend/interpreter/analyzer/grammar.py
@@ -9,15 +9,30 @@ reserved = {
 }
 
 #lista de tokens
-tokens = ['PARA', 'PARC', 'DOT', 'STRING'] + list(reserved.values())
+tokens = ['PARA', 'PARC', 'DOT', 'STRING', 'NUMBER', 'FLOAT', 'BOOLEAN', 'CHAR'] + list(reserved.values())
 
 
-#definicion de tokens
+#-----------------------------------------------------definicion de tokens
+t_PARA = r'\('
+t_PARC = r'\)'
 t_CONSOLE = r'console'
 t_LOG = r'log'
 t_DOT = r'\.'
-t_PARA = r'\('
-t_PARC = r'\)'
+
+def t_CHAR(t):
+    r'[^\t\n\r\f ]'
+    t.type = reserved.get(t.value, 'CHAR')
+    return 
+
+
+def t_BOOLEAN(t):
+    r'true|false'
+    print(f'valor booleano: {t.value}')
+    if t.value == 'true':
+        t.value = True
+    else:
+        t.value = False
+    return t
 
 
 def t_STRING(t):
@@ -26,8 +41,27 @@ def t_STRING(t):
         t.value = str(t.value)
     except ValueError:
         print(f'error al parsear el valor: {t.value}')
-        t.value = ''
+        t.value = None
     return t
+
+def t_FLOAT(t):
+    r'\d+\.\d+'
+    try:
+        t.value = float(t.value)
+    except ValueError:
+        print(f'error al parsear el valor: {t.value}')
+        t.value = None
+    return t
+
+def t_NUMBER(t):
+    r'\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print(f'error al parsear el valor: {t.value}')
+        t.value = None
+    return t
+
 
 def t_newline(t):
     r'\n+'
@@ -37,6 +71,9 @@ def t_error(t):
     print(f'Error lexico {t.value}')
     t.lexer.skip(1) # recuperacion del error
 
+
+
+#--------------------------------------------------definicion de la gramatica
 
 def p_start(p):
     '''start : instrucciones '''
@@ -49,9 +86,22 @@ def p_instruccion(p):
     '''instruccion : print '''
 
 def p_print(p):
-    '''print : CONSOLE DOT LOG PARA STRING PARC'''
-
+    '''print : CONSOLE DOT LOG PARA EXPRESSION PARC'''
     print(p[5])
+
+def p_expression(p):
+    '''EXPRESSION : PRIMITIVO '''
+
+    p[0] = p[1]
+
+def p_primitivo(p):
+    '''PRIMITIVO : STRING 
+                 | NUMBER 
+                 | FLOAT
+                 | BOOLEAN
+                 | CHAR'''
+
+    p[0] = p[1]
 
 
 def p_error(p):
@@ -65,6 +115,6 @@ def parse(input_text):
     lexer = lex.lex() #lexico
     parser = yacc.yacc() #sintactico
 
-    print(input_text)
+    print(f'texto de entrada: {input_text}')
     result = parser.parse(input_text)
     return result
--- backend/main.py
+++ backend/main.py
@@ -2,5 +2,5 @@ from interpreter.analyzer import grammar
 
 
 if __name__ == "__main__":
-    input_text = 'console.log("hola mundo")'
+    input_text = 'console.log("hola \\n mundo")'
     resultado = grammar.parse(input_text)
